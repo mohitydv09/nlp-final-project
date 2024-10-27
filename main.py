ba0cae5cd2e618eadf811832d9f39cb4f2d1dafa
Mohit <mohitydv09@gmail.com>
2024-10-27 13:13:04 -0500

    functions to get world coordinated inplemented, there is bug in the code which needs attentaion later.

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import cv2
 
-from camera import CameraInput
-from object_detector import ObjectDetector
+from camera_input import cameraInput
+from object_detector import objectDetector
 from LLM import LLM
 from realsense_camera import RSCamera
 
 
 def main():
     """Will Import all the classes and functions from the other files and run the program"""
-    camera_input = CameraInput()  # Create an instance of the CameraInput class
-    object_detector = ObjectDetector()  # Create an instance of the ObjectDetector class
-
-    for i in range(500):
-        frame = camera_input.get_rgb_frame()
-        object_detector = object_detector.detect(frame)
-        
+    camera_input = cameraInput()  # Create an instance of the CameraInput class
+    object_detector = objectDetector(model_name="yolo11n.pt", model_folder="./models")  # Create an instance of the ObjectDetector class
 
 if __name__ == "__main__":
     main()
--- object_detector.py
+++ object_detector.py
@@ -1,151 +1,121 @@
-
-class ObjectDetector:
-    def __init__(self):
-        raise NotImplementedError
-    
-if __name__ == "__main__":
-    pass
-
-
-## TESTING ##
-import cv2
-import numpy as np
-
-data_dict = np.load('data/data.npy', allow_pickle=True).item()
-
-# print(data_dict.keys()) 
-
-# first_frame = (data_dict["data"][100,:,:,:3]/255).astype(np.uint8)
-first_frame = (data_dict["data"][100, :, :, :3]).astype(np.uint8)
-depth_frame = data_dict["data"][0,:,:,3]
-
-# print("Shape of data: ", data_dict["data"].shape)
-
-# print(np.max(first_frame))
-# print(np.min(first_frame))
-
-# print(np.max(depth_frame))
-# print(np.min(depth_frame))
-# print(np.median(depth_frame))
-
-
-# cv2.imshow("frame", first_frame)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-##############################################################################################
-# Importing the YOLO v11 shit to detect objects
-# Going to use a standard model, not fine tune another
-from ultralytics import YOLO
-from PIL import Image
-from urllib.request import urlopen
-import numpy as np
-import cv2
 import os
-import matplotlib.pyplot as plt
-import time
+import numpy as np
+
+from ultralytics import YOLO
+
+from camera_input import cameraInput
 
 class objectDetector:
-    def __init__(self, model_name, model_folder):
-        # Ensure model directory exists
+    def __init__(self, model_name='yolo11n.pt', model_folder='./models')->None:
+        self.model = self.load_model(model_name='yolo11n.pt', model_folder='./models')
+
+    def load_model(self, model_name='yolo11n.pt', model_folder='./models')->YOLO:
         os.makedirs(model_folder, exist_ok=True)
-
-        # Construct the model path
         model_path = os.path.join(model_folder, model_name)
-        print(f"Model path: {model_path}")
 
-        # Check if model file exists; if not, download and save it
         if not os.path.exists(model_path):
             try:
-                print(f"Model {model_name} not found. Downloading...")
-                self.model = YOLO(model_name)  # Download model by name
-                self.model.save(model_path)  # Save the model to the specified path
+                print(f"Model {model_name} not found in {model_folder}. Downloading...")
+                model = YOLO(model_name)  # Download model by name
+                model.save(model_path)  # Save the model to the specified path
                 print("Model download complete.")
             except Exception as e:
                 print(f"Error downloading model: {e}")
                 raise FileNotFoundError(f"The model {model_name} could not be downloaded.")
         else:
             print(f"Using existing model: {model_name}")
-            self.model = YOLO(model_path)  # Load the existing model
+            model = YOLO(model_path)
+        return model
 
-    def get_bounding_boxes(self, rgb_image, conf=0.2, show_plt=False, printouts=False):
-        t_start = time.time()
+    def detect_objects(self, 
+                       rgb_image:np.ndarray, 
+                       device = 'cuda:0', 
+                       verbose=False, 
+                       visualization=False, 
+                       save_image=False)->tuple:
+        """Get bounding boxes from the image using the YOLO model.
+        Input Image should be as UltraLytics expects (HWC format): https://docs.ultralytics.com/modes/predict/#inference-sources
+        """
+        result = self.model.predict(rgb_image,
+                                    verbose=verbose, 
+                                    conf = 0.6, 
+                                    iou=0.4, 
+                                    device = device, 
+                                    half=True)[0]
+         ## Result is a list of size as many images are passed, we only will pass one so we need one result.
+        class_ids = result.boxes.cls
+        labels = [result.names[int(cls_id)] for cls_id in class_ids] ## List auto sends it to CPU
+        bboxes_xyxyn = result.boxes.xyxyn.tolist()  ## List auto sends it to CPU
+        if visualization: result.show()
+        if save_image: result.save()
+        return labels, bboxes_xyxyn
 
-        # Predict bounding boxes for the provided image
-        results = self.model.predict(rgb_image, conf=conf)
-
-        detected_objects = []
-
-        # Annotate and display results
-        for result in results:
-            if show_plt:
-                annotated_frame = result.plot()
-                plt.imshow(annotated_frame)
-                plt.axis('off')
-                plt.show()
-            
-        
-            for box in result.boxes:
-                class_id = int(box.cls.item())  # Convert tensor to int
-                # Inspect box.xyxy structure
-                # print(f"box.xyxy: {box.xyxy}, type: {type(box.xyxy)}")
-
-                # Convert coordinates to integers
-                coordinates = box.xyxy.flatten().tolist() if hasattr(box.xyxy, 'flatten') else box.xyxy.tolist()
-                obj_data = {
-                    'label_id': class_id,
-                    'label_name': self.model.names[class_id],
-                    'confidence': box.conf.item(),
-                    'coordinates': [int(coord) for coord in coordinates],  # Convert coordinates to integers
-                    'depth': None # Convert coordinates to integers
-                }
-                # print(obj_data)
-                detected_objects.append(obj_data)
-        
-        t_end = time.time()
-        if printouts:
-            print(f"Time to extract bounding boxes: {t_end - t_start:.2f} seconds")
-        
-        return detected_objects
-
-    def get_depth_data(self, depth_image, bounding_boxes):
-        for obj in bounding_boxes:
-            # Extract bounding box coordinates
-            x1, y1, x2, y2 = obj['coordinates']
-            
-            # Ensure the bounding box coordinates are within the image dimensions
-            x1, x2 = max(0, x1), min(depth_image.shape[1], x2)
-            y1, y2 = max(0, y1), min(depth_image.shape[0], y2)
-            
-            # Extract depth data for the bounding box region
-            depth_values = depth_image[y1:y2, x1:x2]
-
-            # Filter for valid depth values greater than 0
-            valid_depth_values = depth_values[depth_values > 0]
-            
-            if valid_depth_values.size > 0:  # Check if there are valid depth values
-                # Grab 25 random depth values from the region
-                depth_values_sample = np.random.choice(valid_depth_values, size=min(25, valid_depth_values.size))
-                # Compute the median depth value for the region
-                median_depth = np.median(depth_values_sample)
-                obj['depth'] = median_depth * 0.00025 # Convert to meters
+    def get_depth_data(self, 
+                       depth_image:np.ndarray, 
+                       bounding_boxes:list)->list:
+        depth_values = []
+        for bbox in bounding_boxes:
+            x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
+            ## Sample random points in the bounding box, and scale to get pixel vales
+            x_random = np.random.uniform(x1, x2, 5)*depth_image.shape[1]
+            y_random = np.random.uniform(y1, y2, 5)*depth_image.shape[0]
+            ## Get depth values at random points
+            random_depths = [depth_image[int(y), int(x)] for x, y in zip(x_random, y_random)]
+            random_depths = [depth for depth in random_depths if depth > 0]  ## Remove zero values
+            if len(random_depths) > 0:
+                depth_values.append(np.median(random_depths))
             else:
-                obj['depth'] = None  # Or set to a default value, like 0
+                print("This is a bug, do something about this.")
+                depth_values.append(0)
+        return depth_values
+    
+    def pixel2world(self, 
+                    pixel_coordinates:list, 
+                    depth:list, 
+                    ppx:float,
+                    ppy:float,
+                    fx:float,
+                    fy:float)->list:
+        """Convert pixel coordinates to world coordinates using depth information."""
+        world_coordinates = []
+        for i in range(len(depth)):
+            # Convert pixel coordinates to normalized device coordinates
+            x_ndc = (pixel_coordinates[i][0] - ppx) / fx
+            y_ndc = (pixel_coordinates[i][1] - ppy) / fy
 
-        return bounding_boxes
+            # Get depth value
+            z = depth[i]
+
+            # Convert to world coordinates
+            x_world = z * x_ndc
+            y_world = z * y_ndc
+            world_coordinates.append((int(x_world), int(y_world), int(z)))
+        return world_coordinates
+    
+    def get_objects_with_location(self,
+                                   rgb_image:np.ndarray, 
+                                   depth_image:np.ndarray, 
+                                   intrinsics:dict)->tuple:
+        """Get objects with their 3D locations."""
+        labels, bboxes = self.detect_objects(rgb_image)
+        depth_values = self.get_depth_data(depth_image, bboxes)
+
+        pixel_coordinates = [((bbox[0]+bbox[2])//2 , (bbox[1]+bbox[3])//2) for bbox in bboxes] 
+        world_coordinates = self.pixel2world(pixel_coordinates, depth_values, 
+                                              intrinsics['ppx'], intrinsics['ppy'], 
+                                              intrinsics['fx'], intrinsics['fy'])
+        return labels, pixel_coordinates, world_coordinates
         
+if __name__ == "__main__":
+    object_detector = objectDetector()  # Create an instance of the ObjectDetector class
+    camera_input = cameraInput()  # Create an instance of the CameraInput class
+    intrinsics_dict = camera_input.intrinsics
+    depth_scale = camera_input.image_details["depth_scale"]
+    for i in range(5):
+        frame = camera_input.get_frame()
+        rgb_frame = frame[:,:,:3].astype(np.uint8)
+        depth_frame = frame[:,:,3] * depth_scale
 
-# Test with a sample image from the internet
-# url = 'https://ultralytics.com/images/zidane.jpg'
-# img = Image.open(urlopen(url))
-# rgb_frame = np.array(img)
-rgb_frame = np.array(first_frame)
-depth_frame = np.array(depth_frame)
-
-object_detector = objectDetector(model_name="yolo11n.pt", model_folder="./models")
-
-objects = object_detector.get_bounding_boxes(rgb_frame, printouts=True)
-
-objects = object_detector.get_depth_data(depth_frame, objects)
-
-print(objects)
+        labels, pixel_coordinates, world_coordinates = object_detector.get_objects_with_location(rgb_frame, depth_frame, intrinsics_dict)
+        print("Labels:", labels)
+        print("World Coordinates:", world_coordinates)  # Print the world coordinates of detected objects
